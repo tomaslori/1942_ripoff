8fff49f1bb7788044bfe1e5760c565c68d7bf395
tomaslori <tomas_lori142@hotmail.com>
2014-08-23 22:03:35 -0300

    + AbstractEnemy + TankEnemy

---
+++ Assets/Resources/Scripts/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class EnemyController : MonoBehaviour {
+	
+	protected Rigidbody2D body;
+	//protected ObjectManagmentPool<Buildings> buildings;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		Vector3 baseDir = patternMove (body);
+		Vector3 avoidance; // = avoidCollisions (body.position, buildings.getList ());
+		moveSelf (baseDir + avoidance);
+	}
+
+	protected abstract Vector3 patternMove ( Rigidbody2D body );
+
+	protected Vector3 avoidCollisions( Vector3 pos, List<Vector3> hazardousObjects ) {
+		Vector3 avoidance = new Vector3 (0, 0, 0);
+
+		foreach( Vector3 vec in hazardousObjects )
+			avoidance = avoidance + (pos-vec);
+
+		return avoidance;
+	}
+
+	protected void moveSelf ( Vector3 dir ) {
+		body.velocity = dir;
+		body.rotation = dir;
+	}
+}
---
+++ Assets/Resources/Scripts/TankController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankController : EnemyController {
+
+	Vector3 patternMove ( Rigidbody2D body ) {
+		float x = Mathf.Round (Mathf.Sin (Time.deltaTime));
+		float y = -Mathf.Abs (Mathf.Round (Mathf.Sin (Time.deltaTime)));
+		return new Vector3( x, y, 0.0f );
+	}
+
+}
