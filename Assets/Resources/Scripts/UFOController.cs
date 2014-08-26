cb74b82c373e06b732893ff3be79412277320ccc
tomaslori <tomas_lori142@hotmail.com>
2014-08-26 03:16:49 -0300

    UFO

---
+++ Assets/Resources/Scripts/UFOController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UFOController : EnemyController {
+	public float rad = 3.0f;
+
+	protected override Vector3 patternMove () {
+		float x = Mathf.Sin(Time.deltaTime)*rad;
+		float y = topSpd / 2.0f + Mathf.Cos(Time.deltaTime)*rad;
+		return new Vector3( x, y, 0.0f );
+	}
+
+}
